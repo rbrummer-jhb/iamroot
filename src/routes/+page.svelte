bf6eb46a326e8b4ce40de620922775f34ff144b6
Nic <nicolaas@elements.cloud>
2024-06-27 19:02:59 +0200

    Deleted old files and uploaded new files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,394 @@
+<script>
+  import './styles.css'
+  import IcRoundPerson from '~icons/ic/round-person';
+  import IcRoundApps from '~icons/ic/round-apps';
+  import IcRoundBook from '~icons/ic/round-book';
+  import IcRoundAddchart from '~icons/ic/round-addchart';
+  import IcRoundGamepad from '~icons/ic/round-gamepad';
+  import IcBaselineDesignServices from '~icons/ic/baseline-design-services';
+  import IcRoundStar from '~icons/ic/round-star';
+  import IcRoundStarBorder from '~icons/ic/round-star-border';
+  import IcRoundCodeOff from '~icons/ic/round-code-off';
+  import IcRoundMemory from '~icons/ic/round-memory';
+
+  import SkillIconsFigmaDark from '~icons/skill-icons/figma-dark';
+  import SkillIconsIllustrator from '~icons/skill-icons/illustrator';
+  import SkillIconsPhotoshop from '~icons/skill-icons/photoshop';
+  import SkillIconsAftereffects from '~icons/skill-icons/aftereffects';
+
+  import SkillIconsVscodeDark from '~icons/skill-icons/vscode-dark';
+  import SkillIconsMongodb from '~icons/skill-icons/mongodb';
+  import SkillIconsNodejsDark from '~icons/skill-icons/nodejs-dark';
+  import SkillIconsAngularDark from '~icons/skill-icons/angular-dark';
+  import SkillIconsJavaDark from '~icons/skill-icons/java-dark';
+  import SkillIconsJavascript from '~icons/skill-icons/javascript';
+  import SkillIconsTypescript from '~icons/skill-icons/typescript';
+  import SkillIconsPythonDark from '~icons/skill-icons/python-dark';
+  import SkillIconsGit from '~icons/skill-icons/git';
+  import SkillIconsBootstrap from '~icons/skill-icons/bootstrap';
+  import SkillIconsHtml from '~icons/skill-icons/html';
+  import SkillIconsCss from '~icons/skill-icons/css';
+  import SkillIconsFlutterDark from '~icons/skill-icons/flutter-dark';
+  import SkillIconsReactDark from '~icons/skill-icons/react-dark';
+  import SkillIconsC from '~icons/skill-icons/c';
+  import SkillIconsCs from '~icons/skill-icons/cs';
+
+  import SkillIconsTerraformDark from '~icons/skill-icons/terraform-dark';
+  import SkillIconsAwsDark from '~icons/skill-icons/aws-dark';
+  import SkillIconsDocker from '~icons/skill-icons/docker';
+  import SkillIconsKubernetes from '~icons/skill-icons/kubernetes';
+  import SkillIconsGcpDark from '~icons/skill-icons/gcp-dark';
+  import SkillIconsBitbucketDark from '~icons/skill-icons/bitbucket-dark';
+  import SkillIconsLinuxDark from '~icons/skill-icons/linux-dark';
+  import SkillIconsWindowsDark from '~icons/skill-icons/windows-dark';
+  import SkillIconsPowershellDark from '~icons/skill-icons/powershell-dark';
+  import SkillIconsPostman from '~icons/skill-icons/postman';
+  import SkillIconsRedisDark from '~icons/skill-icons/redis-dark';
+  import SkillIconsCloudflareDark from '~icons/skill-icons/cloudflare-dark';
+  import SkillIconsVimDark from '~icons/skill-icons/vim-dark';
+</script>
+
+<div class="content-wrapper">
+  <nav>
+    <a class="btn-algolia-1" href="#about"><IcRoundPerson /><span class="tooltip">About Me</span></a>
+    <a class="btn-algolia-1" href="#skills"><IcRoundApps /><span class="tooltip">Skills</span></a>
+    <a class="btn-algolia-1" href="#certifications"><IcRoundBook /><span class="tooltip">Certifications</span></a>
+    <a class="btn-algolia-1" href="#experience"><IcRoundAddchart /><span class="tooltip">Experience</span></a>
+    <a class="btn-algolia-1" href="#hobbies"><IcRoundGamepad /><span class="tooltip">Hobbies</span></a>
+  </nav>
+  
+  <div class="about-wrapper">
+    <div class="name-title">
+      <p class="terminal-text"><span class="terminal-dir">~/iamr0x0t # </span>whoami<span class="blinking-cursor">_</span></p>
+      <div class="caret"></div>
+      <h3><strong><span class="highlighted-word">Nic</span></strong>olaas Johanner Roodt Brümmer</h3>
+      <div class="role-text">
+        <p>Designer</p>
+        <p><strong>⫸</strong></p>
+        <p>Developer</p>
+        <p><strong>⫸</strong></p>
+        <p><strong><span class="highlighted-word">DevOps</span></strong></p>
+      </div>
+    </div>
+
+    <div class="picture">
+      <img src="" alt="">
+    </div>
+  </div>
+
+  <div class="about-content">
+    <div class="overflow-top"></div>
+    <h1 id="about">About Me</h1>
+    <p>
+      Welcome to my resume.<br><br>
+      Graphic and web designer turned Full Stack Developer turned DevOps Engineer with a passion for
+      building with technology and automation. I often spend my time reading on daily.dev, taking my bike for
+      a ride, lifting weights, rock climbing, playing the occasional video game, or learning something new
+      in the tech space.<br><br>
+      I started out as a freelance graphic designer. I designed new sports apparel for Momentum Health
+      Sports Club and Warrior Sports Academy. I later applied at WeThinkCode_ where I attained a higher
+      certificate as a full-stack developer. I did an internship at BBD as a frontend developer and worked for
+      Elements.Cloud as a backend developer. I later moved into the cloud space at +OneX where I am
+      currently working as a DevOps Engineer with both private and public cloud platforms.
+    </p>
+    <h1 id="skills">Skills</h1>
+    <p>1 <IcRoundStar/> I haven't touched this in ages.<br>
+      2 <IcRoundStar/> I've had some exposure.<br>
+      3 <IcRoundStar/> I've used this in a few projects.<br>
+      4 <IcRoundStar/> I've used this in <strong>many</strong> projects.<br>
+      5 <IcRoundStar/> I use this every day. </p>
+    <h2><IcBaselineDesignServices /> Designer</h2>
+    <div class="skills-wrapper">
+      <div class="skill-tile">
+        <h3>Figma</h3>
+        <h3><SkillIconsFigmaDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Illustrator</h3>
+        <h3><SkillIconsIllustrator /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Photoshop</h3>
+        <h3><SkillIconsPhotoshop /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>After Effects</h3>
+        <h3><SkillIconsAftereffects /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+    </div>
+
+    <h2><IcRoundCodeOff /> Developer</h2>
+    <div class="skills-wrapper">
+      <div class="skill-tile">
+        <h3>VS Code</h3>
+        <h3><SkillIconsVscodeDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>MongoDB</h3>
+        <h3><SkillIconsMongodb /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Node.JS</h3>
+        <h3><SkillIconsNodejsDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Angular</h3>
+        <h3><SkillIconsAngularDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Java</h3>
+        <h3><SkillIconsJavaDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>JavaScript</h3>
+        <h3><SkillIconsJavascript /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>TypeScript</h3>
+        <h3><SkillIconsTypescript /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Python</h3>
+        <h3><SkillIconsPythonDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Git</h3>
+        <h3><SkillIconsGit /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Bootstrap</h3>
+        <h3><SkillIconsBootstrap /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>HTML</h3>
+        <h3><SkillIconsHtml /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>CSS</h3>
+        <h3><SkillIconsCss /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Flutter</h3>
+        <h3><SkillIconsFlutterDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>React</h3>
+        <h3><SkillIconsReactDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>C</h3>
+        <h3><SkillIconsC /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>C#</h3>
+        <h3><SkillIconsCs /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+    </div>
+    
+    <h2><IcRoundMemory /> DevOps</h2>
+    <div class="skills-wrapper">
+      <div class="skill-tile">
+        <h3>Figma</h3>
+        <h3><SkillIconsFigmaDark /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Illustrator</h3>
+        <h3><SkillIconsIllustrator /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>Photoshop</h3>
+        <h3><SkillIconsPhotoshop /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+      <div class="skill-tile">
+        <h3>After Effects</h3>
+        <h3><SkillIconsAftereffects /></h3>
+        <div class="stars">
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStar/></p>
+          <p><IcRoundStarBorder/></p>
+          <p><IcRoundStarBorder/></p>
+        </div>
+      </div>
+    </div>
+  <h1 id="certifications">Certifications</h1>
+  <h1 id="experience">Experience</h1>
+  <h1 id="hobbies">Hobbies</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eum cupiditate eos assumenda odit fugiat modi dolorum recusandae doloribus veniam, repellendus blanditiis corporis reprehenderit architecto ab quidem praesentium nobis mollitia.</p>
+  <div class="overflow-bottom"></div>
+  </div>
+</div>
+<div class="background-gradient">
+  <div class="background-gradient-cover"></div>
+</div>
+<div class="background-gradient-l">
+  <div class="background-gradient-cover-l"></div>
+</div>
