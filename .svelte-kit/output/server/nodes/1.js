c99570eba2a112609c3f4e707658f54ceb4349bc
Nic <67002632+rbrummer-jhb@users.noreply.github.com>
2025-01-18 08:42:34 +0200

    Delete .svelte-kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CjwpvZu7.js","_app/immutable/chunks/scheduler.Cv9TUz2p.js","_app/immutable/chunks/index.C8AfICH1.js","_app/immutable/chunks/entry.CljA9LH4.js"];
-export const stylesheets = [];
-export const fonts = [];
